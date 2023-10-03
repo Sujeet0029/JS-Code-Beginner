1f6bb55703a1875357bb7a395ad2ec73c8bf93cb
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-03 18:11:35 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,18 @@
+name = "Sujeet"
+age = 20
+let isLoggedIn = false
+
+//Number = 2 the power 53
+//bigInt
+//string => ""
+//boolean => true, false
+//null => standlone value
+//undefined
+//symbole => unique
+
+//object
+
+console.log(typeof name)
+console.log(typeof null) // type of null is object
+console.log(typeof undefined)// type of undefined is undefined
+
