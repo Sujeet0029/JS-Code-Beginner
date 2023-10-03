8445633cf6a618b55889f2b462c6fa4344b09910
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-03 18:40:46 +0000

    conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "45"
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber)
+
+//"45" => 45
+//"45dfh" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Sujeet" => true
