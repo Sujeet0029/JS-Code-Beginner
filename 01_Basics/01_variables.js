60c12ab54a9f3d6be0c012bd211473709a5386e3
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-03 17:51:31 +0000

    discussion on variables

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -3,4 +3,7 @@ let userEmail = "sujeetsingh0029@gmail.com"
 var userPassword = "876562"
 userCity = "Siwan"
 
+// userId = 374365 // a constant value can't be changed
+
+
 console.table([userId,userEmail,userPassword,userCity])
