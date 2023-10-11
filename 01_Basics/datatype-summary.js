dd69d9e1559fd0376a38843fc9e2774bf36378d7
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-11 04:34:36 +0000

    Summary

---
+++ 01_Basics/datatype-summary.js
@@ -0,0 +1,23 @@
+//Primitive
+//7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const isLoggedIn = true
+const outsideTemp = null
+let userEmail;
+const id = ('123')
+const bigNumber = 121314142316556787675n
+
+//Non-Primitive(Referance)
+//Array, Object, Function
+
+const superHeros = [Shaktiman, Krish, Meander]
+let myObj ={
+    name: "Sujeet Singh",
+    age: 22,
+    roll: 1
+}
+
+const myFunction = function(){
+    console.log(Hello, World)
+}
