2af10c60757246c5570eb792a6479fa78cd3f692
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-16 10:25:44 +0000

    nums & math

---
+++ 01_Basics/05_nums_math.js
@@ -0,0 +1,26 @@
+// const score = 100
+// console.log(score);
+
+// const balance = new Number(400)
+// console.log(balance);
+
+// console.log(score.toFixed(2));
+
+
+// const otherNumber = 423.32351
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('em-IN'));
+
+//**************************************MATH************************************** */
+ 
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,2,50,8,0));
+// console.log(Math.max(4,2,50,8,0));
+
+// console.log(Math.random());
+console.log((Math.random()*5)+1);
