79f6ad4d91a54f7bf362d0e9e2d044b84c86152c
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-18 13:29:44 +0000

    Dates in JS

---
+++ 01_Basics/06_datesinJS.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+//console.log(myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString());
+console.log(myDate.getTimezoneOffset('18-10-2023'));
