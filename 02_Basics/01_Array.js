0fd2e42254dabc09bba80683995fd52a04a7f47e
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-29 17:06:18 +0000

    Array

--- 02_Basics/01_Array.js
+++ 02_Basics/01_Array.js
@@ -1,2 +1,15 @@
+//Array
 const numbers = [1,2,3,4,5,6,7]
-console.log(numbers.length);
+const myHeros = ["shaktiman","naagraj"]
+//console.log(numbers.splice(0,2));
+//console.log(numbers);
+
+//Array Methods
+// numbers.push(8)
+// numbers.pop()
+
+
+// numbers.unshift(10)
+// numbers.shift()
+
+console.log(numbers);
