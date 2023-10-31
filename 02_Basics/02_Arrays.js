dcf87a0dac8d740189931c4f6e3ebc6bd1a31308
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-10-31 17:54:49 +0000

    Arrays

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,13 @@
+const Marvel_Heroes = ["SPIDER-MAN","DEADPOOL","CAPTAIN-AMERICA","BLACK-PANTHER"]
+const DC_Heroes = ["BATMAN","AQUAMAN","BARRY-ALLEN","GREEN-ARROW"]
+
+// Marvel_Heroes.push(DC_Heroes)
+// console.log(Marvel_Heroes);
+
+//1st method
+// const all_Heroes = Marvel_Heroes.concat(DC_Heroes)
+// console.log(all_Heroes);
+
+//2nd method
+// const all_new_heroes = [...Marvel_Heroes,...DC_Heroes]
+// console.log(all_new_heroes);
