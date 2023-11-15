a1eb3c51c8001ed5e7381b74554cac0eac8a9f69
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-11-15 05:43:53 +0000

    Functions

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyname(){
+console.log("S");
+console.log("U");
+console.log("J");
+console.log("E");
+console.log("E");
+console.log("T");
+}
+//sayMyname()
+
+function addTwoNumbers(num1,num2){
+    console.log(num1+num2);
+    return addTwoNumbers
+}
+//addTwoNumbers(12,22)
+
