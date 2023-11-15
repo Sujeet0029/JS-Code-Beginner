471a6e01b31ff4a9fdd9c2aa70989ba510873113
Sujeet0029 <146464215+Sujeet0029@users.noreply.github.com>
2023-11-15 06:00:07 +0000

    Functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -14,3 +14,13 @@ function addTwoNumbers(num1,num2){
 }
 //addTwoNumbers(12,22)
 
+function loginUserMesssage(username){
+    if(username===undefined){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMesssage());
+// console.log(loginUserMesssage(""));
+// console.log(loginUserMesssage("Sujeet Singh"));
